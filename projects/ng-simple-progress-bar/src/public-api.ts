5df4f2dc6953faa97ff84e5b8da8f445ae3b05ee
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2020-04-19 18:10:28 +0200

    Auto-generated commit

--- projects/ng-simple-progress-bar/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-simple-progress-bar
- */
-
-export * from './lib/ng-simple-progress-bar.service';
-export * from './lib/ng-simple-progress-bar.component';
-export * from './lib/ng-simple-progress-bar.module';
